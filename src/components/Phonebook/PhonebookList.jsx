573b2fa1c6f40bd71bd69f17e043b7f05424ab3a
Viktoriia Moskalenko <vika01mos@gmail.com>
2023-01-17 01:28:36 +0200

    .

--- src/components/Phonebook/PhonebookList.jsx
+++ src/components/Phonebook/PhonebookList.jsx
@@ -6,6 +6,7 @@ return (
                 {contacts.map(({ name, tel }, index) =>
                 <li key={nanoid()}>
                         <span>{name}: {tel}</span>
+                        <span></span>
                         <button type='button' onClick={()=>onDeleteItem(index)} key = {index}>Delete</button>
                     </li>
                 )}
